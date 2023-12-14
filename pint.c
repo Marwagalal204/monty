d9997307afd149a43d513f20a70aa413e70c5429
marwagalal204 <marwagalal204@gmail.com>
2023-12-14 20:09:40 +0200

    fix

--- pint.c
+++ pint.c
@@ -1,18 +1,15 @@
 #include "monty.h"
 /**
- * pint - prints the value at the top of the stack
- * @stack: a pointer to node
- * @line_num: argument
- */
+ * pint -  prints the value at the top of the stack,
+ * @stack: pointer to the first node
+ * @line_num: number of lines
+*/
 void pint(stack_t **stack, unsigned int line_num)
 {
-stack_t *n_node;
-
-n_node = *stack;
-if (n_node == NULL)
+if (*stack == NULL)
 {
 fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 exit(EXIT_FAILURE);
 }
-printf("%d\n", n_node->n);
+printf("%d\n", (*stack)->n);
 }
